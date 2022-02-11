418cb2182e960e6fb16732e7fe5014b6427e35f6
Oren <orenlevi6@gmail.com>
2022-02-11 14:40:01 +0200

    Coupon Class

--- src/main/java/beans/Coupon.java
+++ src/main/java/beans/Coupon.java
@@ -1,4 +1,130 @@
 package beans;
 
+import java.sql.Date;
+
 public class Coupon {
+    private int id;
+    private int companyID;
+    private Category category;
+    private String title;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    private int amount;
+    private double price;
+    private String image;
+
+    public Coupon(int id, int companyID, Category category, String title, String description, Date startDate, Date endDate, int amount, double price, String image) {
+        this.id = id;
+        this.companyID = companyID;
+        this.category = category;
+        this.title = title;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.amount = amount;
+        this.price = price;
+        this.image = image;
+    }
+
+    public Coupon() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCompanyID() {
+        return companyID;
+    }
+
+    public void setCompanyID(int companyID) {
+        this.companyID = companyID;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "Coupon{" +
+                "ID = " + id +
+                ", Company ID = " + companyID +
+                ", Category = " + category +
+                ", Title = '" + title + '\'' +
+                ", Description = '" + description + '\'' +
+                ", Start Date = " + startDate +
+                ", End Date = " + endDate +
+                ", Amount = " + amount +
+                ", Price = " + price +
+                ", Image = '" + image + '\'' +
+                '}';
+    }
+
 }
