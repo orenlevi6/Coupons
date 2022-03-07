9775a4af8cc744c7547a27d56cf554c5c17c4ad2
Oren <orenlevi6@gmail.com>
2022-03-07 12:21:21 +0200

    Tidying up

--- src/main/java/db/db_manager/DBManagerCategories.java
+++ src/main/java/db/db_manager/DBManagerCategories.java
@@ -6,10 +6,11 @@ public class DBManagerCategories {
     public static final String CREATE_CATEGORIES_TABLE = "CREATE TABLE IF NOT EXISTS `coupons`.`categories` (" +
             "  `id` INT NOT NULL AUTO_INCREMENT," +
             "  `name` VARCHAR(45) NULL," +
-            "  PRIMARY KEY (`id`))";
+            "  PRIMARY KEY (`id`)," +
+            "  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE);";
 
     //Create
-    public static final String ADD_CATEGORY = "INSERT INTO `coupons`.`categories` (`name`) VALUES ( ?)";
+    public static final String ADD_CATEGORY = "INSERT IGNORE INTO `coupons`.`categories` (`name`) VALUES ( ?)";
 
     //Update
     public static final String UPDATE_CATEGORY = " UPDATE `coupons`.`categories` SET name=? WHERE id=? ";
